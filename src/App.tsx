b017045a98fa196b1683c6fb5ccb2ac4649fd4aa
h1karoo <o.filipow@yandex.ru>
2022-12-08 23:46:05 +0700

    UserItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
 
 const App = () => {
+  const users: IUser[] = []
   return (
     <div>
       <Card variant = {CardVariant.primary} width='200px' height='200px'>
           <button>Кнопка</button>
           <div>gdfsgfds</div>
       </Card>
+      <UserList users = {users} />
     </div>
   );
 };
